7e5640afb10a826e8ecbd885e40562804c4070e1
Kamil Galazka <kamil24121999@gmail.com>
2022-05-22 11:30:39 +0200

    simple create and delete added

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 <div>
   <HelloWorld msg="Welcome to my first Vue app - todo list"/>
    <form>
-    <input type='text' placeholder="Write task"/>
+    <input type='text' placeholder="Write task" v-model="tmpTaskName"/>
     <input type='submit' @click="addTask">
   </form>
   <ul v-for="task in tasks" :key="task.id">
       <li>
-        {{task.name}}
+        <SingleTask :task="task" :removeTask="removeTask"/>
       </li>
   </ul>
 </div>
@@ -15,11 +15,12 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import SingleTask from './components/SingleTask.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SingleTask
   },
   data(){
     return{
@@ -27,12 +28,25 @@ export default {
         {id:1,name:"first task",done:false},
         {id:2,name:"second task",done:false},
         {id:3,name:"third task",done:false}
-      ]
+      ],
+      tmpTaskName:"",
+      counter:3
     }
   },
   methods:{
     addTask(e){
       e.preventDefault()
+      console.log(this.tmpTaskName)
+      this.counter++
+      const newTask = {id:this.counter,name:this.tmpTaskName,done:false}
+      this.tasks.push(newTask)
+      this.tmpTaskName = ""
+    },
+    removeTask(index){
+      console.log(`delete task at ${index}`)
+      console.log(this.tasks[1])
+      this.tasks = this.tasks.filter(item=>item.id!==index)
+
     }
   }
 }
---
+++ src/components/SingleTask.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>{{task.name}}</p>
+        <p>{{task.id}}</p>
+        <button @click="removeTask(task.id)">delete task</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'SingleTask',
+        props:{
+            task:Object,
+            removeTask:Function
+        },
+        methods:{
+            
+        }
+    }
+</script>
