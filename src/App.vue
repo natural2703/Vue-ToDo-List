4193df012df3c708e9d5c1cd027870e6bd8a2e98
Kamil Galazka <kamil24121999@gmail.com>
2022-05-22 11:00:12 +0200

    basic template for app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <HelloWorld msg="Welcome to my first Vue app - todo list"/>
+   <form>
+    <input type='text' placeholder="Write task"/>
+    <input type='submit' @click="addTask">
+  </form>
+  <ul v-for="task in tasks" :key="task.id">
+      <li>
+        {{task.name}}
+      </li>
+  </ul>
+</div>
 </template>
 
 <script>
@@ -10,6 +20,20 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      tasks:[
+        {id:1,name:"first task",done:false},
+        {id:2,name:"second task",done:false},
+        {id:3,name:"third task",done:false}
+      ]
+    }
+  },
+  methods:{
+    addTask(e){
+      e.preventDefault()
+    }
   }
 }
 </script>
